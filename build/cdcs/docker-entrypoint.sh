5782530e52bc5c299a7656ec940b2a359a299f34
MA042024 <mohammad.abbas@empa.ch>
2024-05-15 11:56:32 +0200

    Update docker-entrypoint.sh

--- build/cdcs/docker-entrypoint.sh
+++ build/cdcs/docker-entrypoint.sh
@@ -10,5 +10,13 @@ echo "********* Starting Celery... *********"
 /scripts/start-celery-worker.sh $PROJECT_NAME &
 /scripts/start-celery-beat.sh $PROJECT_NAME &
 
+# Run database migrations before starting Django server
+echo "********* Running Database Migrations... *********"
+python manage.py migrate --noinput
+
+# Backup data before upgrading (you may need to adjust the backup location)
+echo "********* Backing up Data... *********"
+python manage.py dumpdata > /srv/curator/data_backup.json
+
 echo "********* Starting Django server... *********"
 /scripts/start-django.sh $PROJECT_NAME $WEB_SERVER
