bc23fa4eb4221f38ed3a907393719d4e312b5f05
Mohammad Abbas <mohammad.abbas@empa.ch>
2025-12-03 10:27:34 +0100

    Update start-celery-worker.sh

--- build/cdcs/scripts/start-celery-worker.sh
+++ build/cdcs/scripts/start-celery-worker.sh
@@ -4,4 +4,4 @@ PROJECT_NAME=$1
 /scripts/wait-psql.sh
 
 echo "********* Starting Celery worker... *********"
-celery -A $PROJECT_NAME worker -E -l info
+celery -A "$PROJECT_NAME" worker -E -l info -c 1
